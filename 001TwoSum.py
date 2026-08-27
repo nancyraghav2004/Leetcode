a44879d62a4f1c04b87608cff6c96cd86ad2fa31
Nancy Raghav (HCL Technologies Corporate Services) <v-nraghav@microsoft.com>
2026-08-27 10:23:03 +0530

    Two Sum solution

---
+++ 001TwoSum.py
@@ -0,0 +1,16 @@
+from typing import List
+
+# Two sum
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = solution.twoSum(nums, target)
+    print(result)  # Output: [0, 1]
